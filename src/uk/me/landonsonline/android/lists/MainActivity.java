500aa4bfeb07b0f767083906abdb64f652fe2c7c
MalcyL <malcolm@landonsonline.me.uk>
2013-04-06 10:34:58 +0100

    Added simple list example

---
+++ src/uk/me/landonsonline/android/lists/MainActivity.java
@@ -0,0 +1,38 @@
+package uk.me.landonsonline.android.lists;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.activity_main, menu);
+	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+	switch (item.getItemId())
+	{
+	case R.id.menu_simple_list:
+	    Intent simpleListIntent = new Intent(this, SimpleListActivity.class);
+	    startActivity(simpleListIntent);
+	    return true;
+	}
+
+	return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ src/uk/me/landonsonline/android/lists/SimpleListActivity.java
@@ -0,0 +1,38 @@
+package uk.me.landonsonline.android.lists;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+ 
+public class SimpleListActivity extends ListActivity {
+ 
+	static final String[] NUMBERS = new String[] { "One", "Two", "Three",
+			"Four", "Five", "Six", "Seven", "Eight",
+			"Nine", "Ten", "Eleven", "Twelve", "Thirteen" };
+ 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+ 
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.simple_list,NUMBERS));
+ 
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+ 
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+			    Toast.makeText(getApplicationContext(),
+				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+ 
+	}
+ 
+}
